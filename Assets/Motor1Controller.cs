6d6ee634eac79504f52230116b49a4cab31bcd35
nullmage <jsky.johnson@gmail.com>
2019-09-23 14:15:15 -0600

    Adds init

---
+++ Assets/Motor1Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor1Controller : MonoBehaviour
+{
+    public KeyCode moveUpKey, moveDownKey;
+    public float armSpeed;
+    void Update()
+    {
+        if (Input.GetKey(moveUpKey))
+        {
+            this.transform.Rotate(new Vector3(0.0f, 0.0f, -armSpeed*Time.deltaTime));
+        }
+        
+        if (Input.GetKey(moveDownKey))
+        {
+            this.transform.Rotate(new Vector3(0.0f, 0.0f, armSpeed*Time.deltaTime));
+            // //move paddle down
+            // Vector3 currPos = transform.position;
+            // Vector3 newPos = new Vector3(currPos.x, currPos.y - armSpeed * Time.deltaTime, currPos.z);
+            // transform.position = newPos;
+        }
+    }
+}
+
+
