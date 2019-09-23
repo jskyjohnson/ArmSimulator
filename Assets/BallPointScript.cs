c47366695d368089fab538ca18eb2d58f6489528
nullmage <jsky.johnson@gmail.com>
2019-09-23 14:40:43 -0600

    Adds a really inefficient pen

---
+++ Assets/BallPointScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPointScript : MonoBehaviour
+{
+    public int MAXVAL;
+    private LineRenderer lr;
+    private int index;
+
+    void Start(){
+        lr = GetComponent<LineRenderer>();
+        index = 0;
+    }
+    
+    void Update(){
+        
+        lr.SetPosition(index, this.transform.position);
+        index++;
+        if(index >= MAXVAL){
+            index = 0;
+        }
+    }
+
+}
